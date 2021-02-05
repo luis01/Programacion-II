24983575472a5da0259927a0dd82a67a46f310c9
Luis Hernandez <sarco_luis@hotmail.com>
2021-02-05 10:28:21 -0600

    Agregando el conversor de Longitud y notificaciones de usuarios con Toas y snackbar

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -3,14 +3,20 @@ package com.ugb.miprimerproyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     TabHost tbhConversores;
@@ -18,11 +24,13 @@ public class MainActivity extends AppCompatActivity {
     TextView tempVal;
     Spinner spnOpcionDe, spnOpcionA;
     conversores miConversor = new conversores();
+    RelativeLayout contenidoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        contenidoView = findViewById(R.id.contenidoView);
         tbhConversores = findViewById(R.id.tbhConversores);
         tbhConversores.setup();
 
@@ -34,14 +42,41 @@ public class MainActivity extends AppCompatActivity {
         btnConvertir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tempVal = (TextView)findViewById(R.id.txtcantidad);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
+                try {
+                    tempVal = (TextView) findViewById(R.id.txtcantidad);
+                    double cantidad = Double.parseDouble(tempVal.getText().toString());
 
-                spnOpcionDe = findViewById(R.id.cboDe);
-                spnOpcionA = findViewById(R.id.cboA);
+                    spnOpcionDe = findViewById(R.id.cboDe);
+                    spnOpcionA = findViewById(R.id.cboA);
+                    tempVal = findViewById(R.id.lblRespuesta);
+                    tempVal.setText("Respuesta: " + miConversor.convertir(0, spnOpcionDe.getSelectedItemPosition(), spnOpcionA.getSelectedItemPosition(), cantidad));
+                }catch (Exception e){
+                    tempVal = findViewById(R.id.lblRespuesta);
+                    tempVal.setText("Por favor ingrese los valores correspondiente");
+                    Toast.makeText(getApplicationContext(), "Por ingrese los valores correspondiente "+ e.getMessage(),Toast.LENGTH_SHORT).show();
+                    /*Snackbar snackbar = Snackbar.make(contenidoView, "Por favor ingrese los valores correspondiente", Snackbar.LENGTH_LONG);
+                    snackbar.show();*/
+                }
+            }
+        });
+        btnConvertir = findViewById(R.id.btnCalcularL);
+        btnConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    tempVal = findViewById(R.id.txtCantidadL);
+                    double cantidad = Double.parseDouble(tempVal.getText().toString());
 
-                tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText( "Respuesta: "+ miConversor.convertir(0, spnOpcionDe.getSelectedItemPosition(),spnOpcionA.getSelectedItemPosition(), cantidad) );
+                    spnOpcionDe = findViewById(R.id.cboDeL);
+                    spnOpcionA = findViewById(R.id.cboAL);
+                    tempVal = findViewById(R.id.lblRespuestaL);
+
+                    tempVal.setText("Respuesta: " + miConversor.convertir(1, spnOpcionDe.getSelectedItemPosition(), spnOpcionA.getSelectedItemPosition(), cantidad));
+                }catch (Exception e){
+                    tempVal = findViewById(R.id.lblRespuestaL);
+                    tempVal.setText("Por favor ingrese los valores correspondiente");
+                    Toast.makeText(getApplicationContext(), "Por ingrese los valores correspondiente "+ e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -51,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
 class conversores{
     double[][] conversor = {
         {1.0,8.75,7.77, 24.03,34.8,611.10},/*Monedas*/
-        {1.0},/*Longitud*/
+        {1.0, 100.0,39.37,3.28},/*Longitud*/
             {1.0}/*Masa*/
     };
     public double convertir(int opcion, int de, int a, double cantidad){
