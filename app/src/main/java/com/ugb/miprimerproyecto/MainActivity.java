6e7e3b1b2a638cb041765d5eef2ffef16f31093f
Luis Hernandez <sarco_luis@hotmail.com>
2021-02-15 12:24:08 -0600

    Uso de grafico tipo pastel en Android

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -22,14 +23,29 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
-public class MainActivity extends AppCompatActivity {
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
 
+public class MainActivity extends AppCompatActivity {
+    PieChartView pieChartView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pieChartView = findViewById(R.id.grpEstadistica);
+        List listData = new ArrayList<>();
+        listData.add(new SliceValue(25, Color.rgb(0, 73, 253  )).setLabel("P1: $25"));
+        listData.add(new SliceValue(15, Color.parseColor("#00822B")).setLabel("P2: $15"));
+        listData.add(new SliceValue(35, Color.RED).setLabel("P3: $35"));
+
+        PieChartData pieChartData = new PieChartData(listData);
+        pieChartData.setHasLabels(true).setValueLabelTextSize(14);
+
+        pieChartView.setPieChartData(pieChartData);
     }
 }
