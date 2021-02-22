cbe96c1afb85657ad0c0ef635d1cc439cc806fcf
Luis Hernandez <sarco_luis@hotmail.com>
2021-02-22 11:48:50 -0600

    terminacion del sensor de proximidad

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -2,18 +2,74 @@ package com.ugb.miprimerproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+    TextView tempVal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tempVal = findViewById(R.id.lblSensorProximidad);
+        activarSensorProximidad();
+    }
+
+    @Override
+    protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+    private void activarSensorProximidad(){
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if( sensor==null ){
+            Toast.makeText(getApplicationContext(), "No dispones de sensor de Proximidad", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                tempVal.setText( "Valor: "+event.values[0] );
+                if( event.values[0]<4 ){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+                } else if( event.values[0]>=4 && event.values[0]<=8 ){
+                    getWindow().getDecorView().setBackgroundColor(Color.parseColor("#FDE7D6"));
+                } else{//solo si es mayor de 8
+                    getWindow().getDecorView().setBackgroundColor(Color.parseColor("#D6FDDF"));
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+    }
+    private void iniciar(){
+        sensorManager.registerListener(sensorEventListener,sensor,2000*1000);
+    }
+    private void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
