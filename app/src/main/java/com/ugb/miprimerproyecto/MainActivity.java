f1cd5f82d390a0e955458385e32e6ae67ec3b2d5
Luis Hernandez <sarco_luis@hotmail.com>
2021-02-26 10:39:10 -0600

    obteniendo y mostrando la posicion exacta con GPS

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -2,9 +2,16 @@ package com.ugb.miprimerproyecto;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -25,9 +32,39 @@ import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    LocationListener locationListener;
+    TextView tempVal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tempVal = findViewById(R.id.lblCoordenadas);
+        obtenerPosicion();
+    }
+
+    private void obtenerPosicion() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            tempVal.setText("No hay permiso de acceder al GPS");
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        mostrarPosicion(location);
+        locationListener = miLocalizacion->mostrarPosicion(miLocalizacion);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0,locationListener);
+    }
+
+    private void mostrarPosicion(Location location) {
+        tempVal.setText("Posicion: Latitud: "+ location.getLatitude() +"; Longitud: "+ location.getLongitude() +"; Altitud: "+ location.getAltitude());
     }
 }
